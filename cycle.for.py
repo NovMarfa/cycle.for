7e83a6d329bc84db8bd62414e2d4d989a6dc572e
NovMarfa <mn211195@gmail.com>
2024-12-07 13:26:19 +0300

    Add files via upload

--- cycle.for.py
+++ cycle.for.py
@@ -1,18 +1,31 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+# for i in range (len (numbers)):
+#     if numbers [i] < 2:
+#         continue                       # если число меньше 2, то перескакивает в начало цикла
+#     is_primes = True                   # объявляется перед проверкой
+#     for j in range (2, numbers [i]):   # проверка, что выбранное число не делится в диапазоне от 2х до num[i]
+#         if numbers [i] % j == 0:       # проверка на простоту/сложные числа
+#             not_primes.append (numbers[i]) # записывает в список со сложными числами
+#             is_primes = False
+#             break
+#     if is_primes:                       # если флаг = True, то записываем в список простых чисел
+#         primes.append (numbers [i])
+# print (primes)
+# print (not_primes)
 
-for i in range (len (numbers)):
-    if numbers [i] < 2:
-        continue                       # если число меньше 2, то перескакивает в начало цикла
+for i in numbers:
+    if i < 2:
+        continue
     is_primes = True
-    for j in range (2, numbers [i]):   # проверка, что выбранное число не делится в диапазоне от 2х до num[i]
-        if numbers [i] % j == 0:       # проверка на простоту/сложные числа
-            not_primes.append (numbers[i]) # записывает в список со сложными числами
-            is_primes = False
-            break
-    if is_primes:                       # если флаг = True, то записываем в список простых чисел
-        primes.append (numbers [i])
-
+    for j in range (2, i):
+       if i % j == 0:
+           is_primes = False
+           break
+    if is_primes:
+       primes.append(i)
+    else:
+       not_primes.append(i)
 print (primes)
 print (not_primes)
